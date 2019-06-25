f0c0b3d12dead544b34c02ede40db80371a1f83e
Iryna_Vavruk <iryna_vavruk@epam.com>
2019-06-25 17:26:24 +0300

    commit to master

--- src/test/java/RESTAssuredClient/NewClassForMasterBranch.java
+++ src/test/java/RESTAssuredClient/NewClassForMasterBranch.java
@@ -2,4 +2,5 @@ package RESTAssuredClient;
 
 public class NewClassForMasterBranch {
     //add new functionality to master branch
+    //want to test conflicts
 }
--- src/test/java/RESTAssuredClient/TestClassForTestBranch.java
+++ src/test/java/RESTAssuredClient/TestClassForTestBranch.java
@@ -2,4 +2,7 @@ package RESTAssuredClient;
 
 public class TestClassForTestBranch {
     //add snth new later
+    //want to test merge conflict
+    //or smth else
+    //want to test conflicts
 }
