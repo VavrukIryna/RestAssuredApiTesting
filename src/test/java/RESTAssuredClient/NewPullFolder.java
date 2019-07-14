c7d1fe89454af962830ba57738557596643178c2
Iryna_vavruk@epam.com <guard4388**>
2019-07-14 23:03:11 +0300

    new file added

---
+++ src/test/java/RESTAssuredClient/NewPullFolder.java
@@ -0,0 +1,10 @@
+package RESTAssuredClient;
+
+import org.testng.annotations.Test;
+
+public class NewPullFolder {
+    @Test
+    public void newTest(){
+        System.out.println("New test");
+    }
+}
